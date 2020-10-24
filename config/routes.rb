d0334c3948a202045642c34c23fff45b3f6bdb57
svenka8 <sriram.venkat@chicagobooth.edu>
2020-10-24 22:27:52 +0000

    this this works

--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,6 @@ Rails.application.routes.draw do
   get("/roll/1/20", { :controller => "dice", :action => "one_twenty" })
   get("/roll/2/20", { :controller => "dice", :action => "two_twenty" })
   get("/roll/3/20", { :controller => "dice", :action => "three_twenty" })
+
+  get("/roll/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "infinity_and_beyond"})
 end
