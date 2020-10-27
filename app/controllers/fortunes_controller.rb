c5f1c46a613af66a5afdeaba681866afd2f826d9
svenka8 <sriram.venkat@chicagobooth.edu>
2020-10-27 23:05:23 +0000

    test

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,8 +1,9 @@
 class FortunesController < ApplicationController
   def horoscopes
     #@horoscope = "As your professional dreams unfold, Aries, you may worry about the downside. First, there are new responsibilities that you might doubt your ability to fulfill. Second, you might be catapulted into an uncomfortable new realm of office politics. Don't let these matters put a damper on your enthusiasm. You have what it takes to fulfill the first concern and the wisdom to avoid the second. Onward and upward."
+    @the_sign = params.fetch("the_sign")
     all_zodiacs=Zodiac.list
-    this_zodiac=all_zodiacs.fetch(:aries)
+    this_zodiac=all_zodiacs.fetch(:@the_sign)
     @horoscope = this_zodiac.fetch(:horoscope)
     @array_of_numbers = Array.new
 
@@ -11,8 +12,10 @@ class FortunesController < ApplicationController
       
       @array_of_numbers.push(another_number)
     end
+    render({ :template => "flame_templates/generic.html.erb" })
 
-  #   render({ :template => "flame_templates/aries.html.erb" })
+
+  #   render({ :template => "flame_templates/generic.html.erb" })
   # end
 
   # def lion
--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,28 @@ Rails.application.routes.draw do
   # PART 1: EACH IN ERB
   # ===================
 
-  get("/", { :controller => "lottery", :action => "lucky"})
-  get("/lottery/lucky", { :controller => "lottery", :action => "lucky"})
-  get("/lottery/unlucky", { :controller => "lottery", :action => "unlucky"})
+  # get("/", { :controller => "lottery", :action => "lucky"})
+  # get("/lottery/lucky", { :controller => "lottery", :action => "lucky"})
+  # get("/lottery/unlucky", { :controller => "lottery", :action => "unlucky"})
 
   # PART 2: RCAV DEBUGGING
   # ======================
 
-  get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
-  get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
-  get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
+  # get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
+  # get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
+  # get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
-  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
-  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  # get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
+  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
-  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
-  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  # get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  # get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
-  get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
-  get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
-  get("/zodiacs/pisces", { :controller => "water", :action => "fish" })
+  # get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
+  # get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
+  # get("/zodiacs/pisces", { :controller => "water", :action => "fish" })
 
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
 
   get("/roll/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "infinity_and_beyond"})
 
-  get("zodiacs/:the_sign", {:controller=> "fortunes",:action => "horoscopes"})
+  get("zodiacs/:the_sign", {:controller=> "fortunes", :action => "horoscopes"})
 end
